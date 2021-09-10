062701fc53fe9544b342ec99048cd7392bc14430
bartuczpetra <bartucz.petra@gmail.com>
2021-09-10 16:35:59 +0200

    kész játék programkód

--- src/kincseslada/Kincseslada.java
+++ src/kincseslada/Kincseslada.java
@@ -3,7 +3,10 @@ package kincseslada;
 public class Kincseslada {
 
     public static void main(String[] args) {
-        
+        Ladak l1 = new Ladak();
+        l1.kiir();
+        l1.kerdes();
+        l1.eldont();
     }
-    
+
 }
--- src/kincseslada/Ladak.java
+++ src/kincseslada/Ladak.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kincseslada;
 
-/**
- *
- * @author hallgato
- */
+import java.util.Scanner;
+
 public class Ladak {
-    
+
+    String[] szin = {"aranyláda", "ezüstláda", "bronzláda"};
+    String[] mondas = {"Bennem van a kincs.", "Nem bennem van a kincs.", "Az arany láda hazudik."};
+    int melyikSzin;
+
+    void kiir() {
+        for (int i = 0; i < szin.length; i++) {
+            System.out.println(" ---------");
+            System.out.println("|" + szin[i] + "|: " + mondas[i]);
+            System.out.println(" ---------");
+        }
+    }
+
+    void kerdes() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Melyik színű ládában van a kincs (arany(1)/ezüst(2)/bronz(3))? ");
+        melyikSzin = sc.nextInt();
+    }
+
+    void eldont() {
+        switch (melyikSzin) {
+            case 1:
+                System.out.println("Nem találtad el, nem az arany ládában van a kincs!");
+                break;
+            case 2:
+                System.out.println("Eltaláltad, az ezüst ládában van a kincs!");
+                break;
+            case 3:
+                System.out.println("Nem találtad el, nem a bronz ládában van a kincs!");
+                break;
+            default:
+                break;
+        }
+    }
 }
